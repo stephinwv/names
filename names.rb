7fe36ca196aa5863d99246073fb68e3d2b0f626d
stephinwv <thekingdomseeker@aol.com>
2017-06-28 14:49:38 -0400

    names first and last

--- names.rb
+++ names.rb
@@ -2,8 +2,18 @@
 def intro()
 	print "My first name is "
 end
+def intro2()
+	print "My last name is "
+end
+def last_name(name)
+	puts name
+end
+
 def first_name(name)
 	puts name
 end
 intro()
+
 first_name("Steph")
+intro2()
+last_name("Frame")
